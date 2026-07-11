6eceb5a209e46ff59aa764f89e30660cc9f9b539
harum4121-ctrl <haru.m4121@icloud.com>
2026-07-11 16:54:53 +0900

    cards.js を更新

--- js/data/cards.js
+++ js/data/cards.js
@@ -74,6 +74,77 @@ const cards = {
 
     cursedObjects: [
 
+        {
+            id: "sukuna_finger",
+
+            name: "宿儺の指",
+
+            type: "呪物",
+
+            effect: {
+                type: "taunt",
+                duration: 1
+            }
+        },
+
+        {
+            id: "death_painting_1",
+
+            name: "受胎九相図 一番",
+
+            type: "呪物",
+
+            effect: [
+                {
+                    type: "enemyCursedDown",
+                    value: 10
+                },
+                {
+                    type: "allyCursedUp",
+                    value: 10
+                }
+            ]
+        },
+
+        {
+            id: "death_painting_2",
+
+            name: "受胎九相図 二番",
+
+            type: "呪物",
+
+            effect: {
+                type: "extraAction",
+                penalty: "stunNextTurn"
+            }
+        },
+
+        {
+            id: "death_painting_3",
+
+            name: "受胎九相図 三番",
+
+            type: "呪物",
+
+            effect: {
+                type: "doubleNextDamage"
+            }
+        },
+
+        {
+            id: "prison_realm",
+
+            name: "獄門疆",
+
+            type: "呪物",
+
+            effect: {
+                type: "sealAfterTurns",
+                delay: 2,
+                duration: 5
+            }
+        }
+
     ],
 
 
