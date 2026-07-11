19fc4739ecf7daf6e42f0e74356d5e44727afa75
harum4121-ctrl <haru.m4121@icloud.com>
2026-07-11 23:39:03 +0900

    enemies.js を作成

---
+++ js/battle/enemies.js
@@ -0,0 +1,33 @@
+const enemies = {
+
+    sukuna: {
+
+        name: "両面宿儺",
+
+        hp: 400,
+
+        maxHp: 400
+
+    },
+
+    mahito: {
+
+        name: "真人",
+
+        hp: 350,
+
+        maxHp: 350
+
+    },
+
+    jogo: {
+
+        name: "漏瑚",
+
+        hp: 320,
+
+        maxHp: 320
+
+    }
+
+};
