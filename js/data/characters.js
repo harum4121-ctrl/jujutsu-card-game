1336914a26da0b6748cab4f05f66d4ea346305d0
harum4121-ctrl <haru.m4121@icloud.com>
2026-07-11 15:49:46 +0900

    characters.js を更新

--- js/data/characters.js
+++ js/data/characters.js
@@ -188,4 +188,246 @@ const characters = {
 
     }
 
-};
+},
+
+
+    gojo: {
+
+        name:"五条悟",
+        type:"術",
+
+        hp:400,
+        maxHp:400,
+
+        cursedPower:60,
+        maxCursedPower:60,
+
+        recovery:10,
+
+
+        skills:[
+
+            {
+                name:"打撃",
+                type:"近接",
+                target:"単体",
+                damage:50,
+                cost:0,
+
+                ct:1
+            },
+
+            {
+                name:"無下限呪術",
+                type:"特殊",
+                target:"味方",
+
+                cost:10,
+
+                effect:{
+                    type:"invincible",
+                    duration:1
+                },
+
+                ct:5
+            },
+
+            {
+                name:"僕、最強だから",
+                type:"特殊",
+                target:"敵全体",
+
+                cost:10,
+
+                effect:{
+                    type:"enemySkillCostUp",
+                    value:10,
+                    duration:1
+                },
+
+                ct:1
+            }
+
+        ],
+
+
+        ultimate:{
+
+            name:"領域展開 無量空処",
+
+            type:"特殊",
+            target:"単体",
+
+            costCard:2,
+
+            effect:[
+                {
+                    type:"cursedDown",
+                    value:50
+                },
+                {
+                    type:"stun",
+                    duration:2
+                },
+                {
+                    type:"damageTakenUp",
+                    value:50
+                }
+            ]
+
+        }
+
+    },
+
+
+    maki:{
+
+        name:"禪院真希",
+        type:"体",
+
+        hp:380,
+        maxHp:380,
+
+        cursedPower:1,
+        maxCursedPower:1,
+
+        recovery:0,
+
+
+        skills:[
+
+            {
+                name:"切り裂き",
+                type:"近接",
+                target:"単体",
+                damage:60,
+                cost:0,
+
+                ct:1
+            },
+
+            {
+                name:"薙ぎ払い",
+                type:"近接",
+                target:"全体",
+                damage:50,
+                cost:0,
+
+                ct:3
+            },
+
+            {
+                name:"骨砕き",
+                type:"近接",
+                target:"単体",
+                damage:20,
+                cost:0,
+
+                effect:{
+                    type:"damageTakenUp",
+                    value:30,
+                    duration:1
+                },
+
+                ct:1
+            }
+
+        ],
+
+
+        ultimate:{
+
+            name:"游雲",
+
+            type:"近接",
+            target:"単体",
+
+            damage:120,
+
+            costCard:1,
+
+            effect:{
+                type:"damageBuffPermanent",
+                value:20
+            }
+
+        }
+
+    },
+
+
+    inumaki:{
+
+        name:"狗巻棘",
+        type:"術",
+
+        hp:350,
+        maxHp:350,
+
+        cursedPower:40,
+        maxCursedPower:40,
+
+        recovery:10,
+
+
+        skills:[
+
+            {
+                name:"爆ぜろ",
+                type:"遠距離",
+                target:"全体",
+                damage:20,
+
+                selfDamage:10
+            },
+
+            {
+                name:"動くな",
+                type:"特殊",
+                target:"2体",
+
+                cost:30,
+
+                effect:{
+                    type:"stun",
+                    duration:1
+                },
+
+                selfDamage:40,
+
+                ct:2
+            },
+
+            {
+                name:"潰れろ",
+                type:"遠距離",
+                target:"単体",
+                damage:80,
+
+                cost:20,
+
+                selfDamage:20
+            }
+
+        ],
+
+
+        ultimate:{
+
+            name:"ぶっ飛べ",
+
+            type:"遠距離",
+            target:"単体",
+
+            damage:120,
+
+            costCard:1,
+
+            effect:{
+                type:"heal",
+                value:80
+            }
+
+        }
+
+    }
