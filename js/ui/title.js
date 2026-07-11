fcbcf800e279f58d25e6a25f2f209159205ce46f
harum4121-ctrl <haru.m4121@icloud.com>
2026-07-11 17:31:04 +0900

    title.js を作成

---
+++ js/ui/title.js
@@ -0,0 +1,35 @@
+function showTitle() {
+
+    const app = document.getElementById("app");
+
+    app.innerHTML = `
+        <div class="title-screen">
+
+            <h1>呪術カードゲーム</h1>
+
+            <p>Version 0.1</p>
+
+            <button id="battleButton">
+                バトル開始
+            </button>
+
+            <button id="deckButton">
+                デッキ編集
+            </button>
+
+            <button id="ruleButton">
+                ルール
+            </button>
+
+        </div>
+    `;
+
+    document
+        .getElementById("battleButton")
+        .addEventListener("click", () => {
+
+            showCharacterSelect();
+
+        });
+
+}
