0ea3e58b6cfe3aed0223ebab3111e3381361aa31
harum4121-ctrl <haru.m4121@icloud.com>
2026-07-12 01:57:15 +0900

    enemySelect.js を作成

---
+++ js/ui/enemySelect.js
@@ -0,0 +1,63 @@
+function showEnemySelectScreen() {
+
+    const app = document.getElementById("app");
+
+    let html = `
+
+    <div class="enemy-select">
+
+        <h1>敵選択</h1>
+
+    `;
+
+
+    for (const id in enemies) {
+
+        const enemy = enemies[id];
+
+
+        html += `
+
+        <div class="character">
+
+            <h2>${enemy.name}</h2>
+
+            <p>
+                HP：${enemy.hp}
+            </p>
+
+            <button onclick="selectEnemy('${id}')">
+
+                この敵と戦う
+
+            </button>
+
+        </div>
+
+        <br>
+
+        `;
+
+    }
+
+
+    html += `
+
+    </div>
+
+    `;
+
+
+    app.innerHTML = html;
+
+}
+
+
+
+function selectEnemy(id) {
+
+    gameState.selectedEnemy = id;
+
+    startBattle();
+
+}
