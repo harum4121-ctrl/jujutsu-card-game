bfca21ffc3e269ec9587dfc079cb7aeb9900b43d
harum4121-ctrl <haru.m4121@icloud.com>
2026-07-12 01:44:45 +0900

    enemies.js を更新

--- js/data/enemies.js
+++ js/data/enemies.js
@@ -1,18 +1,57 @@
 const enemies = {
 
-    sukuna: {
-        name: "両面宿儺",
-        hp: 400
+    curse1: {
+
+        name: "呪霊（低級）",
+
+        hp: 150,
+
+        attack: 20
+
     },
 
-    mahito: {
-        name: "真人",
-        hp: 350
+
+    curse2: {
+
+        name: "呪霊（中級）",
+
+        hp: 300,
+
+        attack: 35
+
     },
+
 
     jogo: {
+
         name: "漏瑚",
-        hp: 320
+
+        hp: 600,
+
+        attack: 60
+
+    },
+
+
+    mahito: {
+
+        name: "真人",
+
+        hp: 500,
+
+        attack: 45
+
+    },
+
+
+    sukuna: {
+
+        name: "両面宿儺",
+
+        hp: 800,
+
+        attack: 100
+
     }
 
 };
