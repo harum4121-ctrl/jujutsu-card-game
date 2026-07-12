26cf8fd1758bd19f47966ba9a075b8d9f116969a
harum4121-ctrl <haru.m4121@icloud.com>
2026-07-12 21:10:23 +0900

    enemies.js を更新

--- js/data/enemies.js
+++ js/data/enemies.js
@@ -6,13 +6,45 @@ const enemies = {
 
         hp: 800,
 
-        attack: 100,
+        attack: 30,
 
-        skills: [],
+        skills: [
 
-        ultimate: null
+            {
+                name: "打撃",
+                damage: 30,
+                target: "単体"
+            },
 
-    },
+            {
+                name: "捌",
+                damage: 80,
+                target: "単体",
+                cost: 20
+            },
+
+            {
+                name: "解",
+                damage: 50,
+                target: "全体",
+                cost: 40
+            }
+
+        ],
+
+        ultimate: {
+
+            name: "開",
+
+            damage: 250,
+
+            target: "単体"
+
+        }
+
+    }
+
+};
 
 
     jogo: {
