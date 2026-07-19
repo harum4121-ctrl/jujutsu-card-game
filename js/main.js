94f082c1650508ca992ed8ef838cc121c1d1e252
harum4121-ctrl <haru.m4121@icloud.com>
2026-07-19 20:21:53 +0900

    main.js を更新

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-alert("テスト");
+
 // データ確認
-alert("①");
 console.log(characters);
-alert("②");
+
 console.log(cards);
-alert("③");
+
 
 // ゲーム全体の状態
 const gameState = {
@@ -28,11 +27,10 @@ const gameState = {
 
 };
 
-alert("④");
+
 
 // ゲーム開始
 window.onload = () => {
-    alert("⑤");
-    alert("main.jsは動いています");
+
     showTitle();
 };
