164e340c12197ae654718f43c610c73a90a12b43
harum4121-ctrl <haru.m4121@icloud.com>
2026-08-07 16:04:35 +0900

    gacha.js を更新

--- js/ui/gacha.js
+++ js/ui/gacha.js
@@ -288,6 +288,19 @@ function performGacha(count) {
 
     saveCollection();
 
+if (
+    typeof playCinematicGacha !== "function"
+) {
+
+    alert(
+        "playCinematicGacha が読み込まれていません"
+    );
+
+    return;
+}
+
+alert("ガチャ演出を開始します");
+
 playCinematicGacha(results);
 
 }
